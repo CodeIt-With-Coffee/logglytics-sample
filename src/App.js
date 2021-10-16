ce460ad5e7948144e81060a7d90cfec7931b316a
SanjayDevTech <sureshsanjay805@gmail.com>
2021-10-17 03:20:16 +0530

    completed sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useMemo } from 'react';
 import './App.css';
+import { default as logglytics } from '@codeitwithcoffee/logglytics';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const config = {
+		apiKey: 'q14V5PRhyLP7oy4MOQEii7px2SFuT4zJ4e88Ml4NRi8Cx7zmmaxzDX-5cbzS9fZt',
+		projectId: 'b6951cfe-184b-4cde-9dc1-122e196ea904',
+	};
+	const logger = useMemo(
+		() => logglytics(config),
+		[config.apiKey, config.projectId],
+	);
+	const featureOne = () => logger.event('FEATURE_1');
+	const featureTwo = () => logger.event('FEATURE_2');
+	const logHello = () => logger.log('INFO', 'Hello');
+
+	return (
+		<div className='App'>
+			<button onClick={featureOne}>Feature 1</button>
+			<button onClick={featureTwo}>Feature 2</button>
+			<button onClick={logHello}>Log hello</button>
+		</div>
+	);
 }
 
 export default App;
